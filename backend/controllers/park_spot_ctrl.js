9a929f1eac1da1187843b711e212f48aaaffa0f2
Bescore <jeanmar-97.1@hotmail.fr>
2022-04-03 11:10:47 +0200

    attributuion de places

--- backend/controllers/park_spot_ctrl.js
+++ backend/controllers/park_spot_ctrl.js
@@ -1,6 +1,27 @@
 const connection = require( "../mysql/db" );
 
 
+
+//attribution de places
+exports.getSpot = ( req, res, next ) => {
+
+    connection.execute( `UPDATE test_tech_second.place_park SET disponibilité=?  WHERE idplace_park=?`, [  `${req.body.userId }`,`${req.body.place}` ],
+        function ( err, result ) {
+            if ( result == '' ) {
+                
+                res.status( 400 ).json( 'erreur' )
+            } else {
+                console.log( req.body )
+                res.status( 200 ).json( "place attribué" )
+            }
+        } )
+
+}
+
+
+
+
+//places disponibles
 exports.findEmpty = ( req, res, next ) => {
     connection.execute( `SELECT * FROM test_tech_second.place_park WHERE disponibilité= ?`, [ `0` ],
         function ( err, result ) {
@@ -9,6 +30,18 @@ exports.findEmpty = ( req, res, next ) => {
                 res.status( 400 ).json( 'erreur, parking plein' )
             } else {
                 res.status( 200 ).json( result )
+            }
+        } )
+}
+//durée d'occupation
+exports.timeRemaining = ( req, res, next ) => {
+    connection.execute( `SELECT timediff(now(),occupation) FROM test_tech_second.place_park WHERE disponibilité=?`, [ `${req.body.userId}` ],
+        function ( err, result ) {
+            if ( result == '' ) {
+                console.log( req.body )
+                res.status( 400 ).json( "cette utilisateur n'existe pas" )
+            } else {
+                res.status( 200 ).json( result[0] )
             }
         } )
 }
--- backend/routes/park_spot.js
+++ backend/routes/park_spot.js
@@ -2,5 +2,7 @@ const express = require( 'express' );
 const park_ctrl = require( '../controllers/park_spot_ctrl' );
 const router = express.Router();
 
+router.put( '/spot', park_ctrl.getSpot );
 router.get('/', park_ctrl.findEmpty );
+router.post( '/time', park_ctrl.timeRemaining );
 module.exports = router;

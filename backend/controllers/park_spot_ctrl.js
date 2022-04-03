fba0248beb9d6c483ea8d731118f4ccfaa45c592
Bescore <jeanmar-97.1@hotmail.fr>
2022-04-03 11:53:06 +0200

    temps d'occup recupéré a la liberation de la place, heure et date d'inscription à la prise de la place

--- backend/controllers/park_spot_ctrl.js
+++ backend/controllers/park_spot_ctrl.js
@@ -2,10 +2,10 @@ const connection = require( "../mysql/db" );
 
 
 
-//attribution de places
+//attribution de places / attendu(place: B2,userId:100)
 exports.getSpot = ( req, res, next ) => {
 
-    connection.execute( `UPDATE test_tech_second.place_park SET disponibilité=?  WHERE idplace_park=?`, [  `${req.body.userId }`,`${req.body.place}` ],
+    connection.execute( `UPDATE test_tech_second.place_park SET disponibilité=?,occupation=?  WHERE idplace_park=?`, [ `${ req.body.userId }`, `${ req.body.time }`,`${req.body.place}` ],
         function ( err, result ) {
             if ( result == '' ) {
                 
@@ -18,7 +18,26 @@ exports.getSpot = ( req, res, next ) => {
 
 }
 
+//libération de la place
 
+exports.freeThespot = ( req, res, next ) => {
+    connection.execute( `SELECT timediff(now(),occupation) FROM test_tech_second.place_park WHERE disponibilité=?`, [ `${ req.body.userId }` ],
+        function ( err, resulted ) {
+            connection.execute( `UPDATE test_tech_second.place_park SET disponibilité=?  WHERE disponibilité=?`, [ `0`, `${ req.body.userId }` ],
+                function hell( err, result ) {
+                    if ( result == '' ) {
+
+                        res.status( 400 ).json( 'erreur' )
+                    } else {
+                        //temps passé sur la place
+                        console.log( req.body )
+                        res.status( 200 ).json( resulted[0] )
+                    }
+                } )
+        } )
+    
+
+}
 
 
 //places disponibles
@@ -33,7 +52,7 @@ exports.findEmpty = ( req, res, next ) => {
             }
         } )
 }
-//durée d'occupation
+//durée d'occupation/ attendu (userId)
 exports.timeRemaining = ( req, res, next ) => {
     connection.execute( `SELECT timediff(now(),occupation) FROM test_tech_second.place_park WHERE disponibilité=?`, [ `${req.body.userId}` ],
         function ( err, result ) {
--- backend/routes/park_spot.js
+++ backend/routes/park_spot.js
@@ -3,6 +3,7 @@ const park_ctrl = require( '../controllers/park_spot_ctrl' );
 const router = express.Router();
 
 router.put( '/spot', park_ctrl.getSpot );
+router.put('/freespot',park_ctrl.freeThespot)
 router.get('/', park_ctrl.findEmpty );
 router.post( '/time', park_ctrl.timeRemaining );
 module.exports = router;

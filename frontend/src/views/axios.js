441b7879c23ed02b85044192f5825a21dea6fbc7
Bescore <jeanmar-97.1@hotmail.fr>
2022-04-18 17:51:56 +0200

    tries

--- frontend/src/views/axios.js
+++ frontend/src/views/axios.js
@@ -3,5 +3,5 @@ const axios = require( 'axios' )
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
-axios.defaults.headers.common[ 'Authorization' ] = 'Bearer ' + localStorage.getItem( 'token' );
+
 

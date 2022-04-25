6f073da9ba165f279ea9d3ebd1b8bc6c85f89e72
jean-marie <jeanmar-97.1@hotmail.fr>
2022-04-25 13:45:48 +0200

    créer le tableau

---
+++ frontend/src/components/payment.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+<h2>Paiement</h2>
+<table>
+<thead>
+    <tr>
+        <th>Places</th>
+        <th>Temps</th>
+        <th>Dû</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td></td>
+    </tr>
+</tbody>
+</table>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style  scoped>
+table{
+    border-collapse: collapse;
+}
+table, thead,th,tr,td{
+    border: solid 1px black;
+    padding: 10px;
+}
+</style>
--- frontend/src/views/payment.vue
+++ frontend/src/views/payment.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-
+        <payment/>
     </div>
 </template>
 
 <script>
+import payment from "@/components/payment.vue";
     export default {
-        
+        name:"payview",
+        components:{
+            payment,
+        }
     }
 </script>
 

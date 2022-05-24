ad99b13eb53438b0e29d2204542d2a72a36ea44b
Bescore <jeanmar-97.1@hotmail.fr>
2022-05-24 19:38:45 +0200

    ajout d'helmet

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require( 'express' );
 const app = express();
+const helmet = require("helmet");
+
+app.use(helmet());
 app.use( ( req, res, next ) => {
     res.setHeader( 'Access-Control-Allow-Origin', '*' );
     res.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization' );
